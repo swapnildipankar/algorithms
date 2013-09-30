0c2b935766285f70c513b1a0f8f4d06434ecc24d
swapnildipankar <dswapnil@gmail.com>
2013-09-30 11:10:12 -0700

    Adding Digital Search Tree (Trie)

---
+++ digital_search_tree/DigitalSearchTree.java
@@ -0,0 +1,70 @@
+public class DigitalSearchTree {
+    private static class Node {
+        public Node[] next;
+        public boolean isTerminalNode;
+
+        Node() {
+            next = new Node[26];
+        }
+    }
+
+    private static class Tree {
+        private Node root;
+
+        Tree() {
+            root = new Node();
+        }
+
+        private int getIntegerValueForCharacter(char inputChar) {
+            return inputChar - 'a';
+        }
+
+        public void insertWord(String inputWord) {
+            Node currentNode = root;
+            for(int idx=0; idx<=inputWord.length(); ++idx) {
+                if(idx == inputWord.length()) {
+                    currentNode.isTerminalNode = true;
+                    return;
+                }
+
+                int integerValueForCharacter = getIntegerValueForCharacter(Character.toLowerCase(inputWord.charAt(idx)));
+                if(currentNode.next[integerValueForCharacter] == null) {
+                    currentNode.next[integerValueForCharacter] = new Node();
+                }
+                currentNode = currentNode.next[integerValueForCharacter];
+            }
+        }
+
+        public boolean isValidWord(String inputWord) {
+            Node currentNode = root;
+
+            for(int idx=0; idx<inputWord.length(); ++idx) {
+                int integerValueForCharacter = getIntegerValueForCharacter(Character.toLowerCase(inputWord.charAt(idx)));
+                if(currentNode.next[integerValueForCharacter] == null) {
+                    return false;
+                }
+                currentNode = currentNode.next[integerValueForCharacter];
+            }
+
+            if(currentNode.isTerminalNode == true) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Tree tree = new Tree();
+
+        tree.insertWord("something");
+        tree.insertWord("abacus");
+        tree.insertWord("absurd");
+        System.out.println(tree.isValidWord("something"));
+        System.out.println(tree.isValidWord("abacus"));
+        System.out.println(tree.isValidWord("absurd"));
+        System.out.println(tree.isValidWord("dodo"));
+
+        System.out.println("Hello World!");
+    }
+}
